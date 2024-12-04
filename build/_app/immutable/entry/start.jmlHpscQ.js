190207a72b10b947ebf56bb8d679f9b6a06827be
Emily Walpole <emily.walpole@protonmail.com>
2024-12-04 02:22:01 -0500

    rename buid to docs

--- build/_app/immutable/entry/start.jmlHpscQ.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.5bV5_3zN.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'docs', // The directory for HTML files
+			assets: 'docs', // The directory for static assets
+			fallback: null      // Optional: SPA fallback file
+		  })
 	}
 };
 export default config;
